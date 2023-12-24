0d321c829a80cba36747cf680a212cf68d56e6af
Paul Martin Enano <123139399+enano1@users.noreply.github.com>
2023-12-24 16:53:06 -0500

    Included files, excluded packages
    pip install requirements.txt should add many of the packages, if not include firebase backages
    excluded .env and clientsecret.json file

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# SpotifyPlaylistCreator
+# project-11
+Members: Paul, Matias, Joey, Shasta, Alan, Joe, Michael, Farzaan
+
+Project Layout:
+
+Write a sentence, and for each word (this requires tokenization), each word will utilize spotify's API to generate a spotify playlist based of those words, then use DOG API's image generator for the playlist cover image.
+
+
+Front-End: HTML, CSS, Angular
+Joe, Alan
+
+Back-end: Python, Flask
+Database: Firebase (Shasta, Joey)
+Spotify: (Michael, Paul)
+DOG API: (Farzaan, Matias)
+
+
---
+++ script.js
@@ -0,0 +1,40 @@
+// Handles the loading animation
+var loader = document.getElementById('loader');
+document.getElementById('submit-button').addEventListener('click', function(event) {
+    // loader.style.display = "block";
+    fadeIn(loader);
+});
+
+// Function to fade in elements
+function fadeIn(element) {
+    let op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    let timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+// Function to fade out elements
+function fadeOut(element) {
+    let op = 1;  // initial opacity
+    let timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+window.onload = function() {
+    let form = document.getElementById('fade');
+    fadeIn(form);
+};
+
